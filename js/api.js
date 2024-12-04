2faf04b8e1580c06b50dd73b92252f71bfc2a5d7
SomratIIT <101197949+SomratIIT@users.noreply.github.com>
2024-12-05 00:42:19 +0600

    api definition

---
+++ js/api.js
@@ -0,0 +1,11 @@
+/* 
+Simple Definition of API
+An API (Application Programming Interface) is a tool that allows two software applications or systems to communicate with each other.
+
+It works like a messenger, sending requests from one system to another and returning the response.
+
+Example:
+When you use a food delivery app to track your order on a map, the app uses an API to get location data from services like Google Maps.
+
+In short: API is the bridge that connects different software to share data and functionality.
+*/
