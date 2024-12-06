d4b82c197cd9bbe9983f97bc51a18eeed6ec113f
SomratIIT <101197949+SomratIIT@users.noreply.github.com>
2024-12-06 20:23:17 +0600

    Nije Nije korlam vai

--- post2.js
+++ post2.js
@@ -4,11 +4,19 @@ function postShow()
     .then(response =>response.json())
     .then(data =>postShowCall(data) )
 }
-
+postShow();
 function postShowCall(users)
-{
+{ const container2 = document.getElementById('div-container2');
    for(const user of users)
    {
     console.log(user);
+    const div = document.createElement('div');
+    div.classList.add('user');
+    div.innerHTML = `
+    <h3>id:${user.id}</h3>
+    <h5>Post: title-${user.title}</h5>
+    <p>Post Description:${user.body}</p>
+    `
+    container2.appendChild(div);
    }
 }
